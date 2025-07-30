7b895281a2c89ffa9ee12b20f41a72cf1aeededf
Kevin Chen <kevin.chen@verkada.com>
2025-07-30 15:21:08 -0700

    add test

--- helloworld/formatting/formatting_test.go
+++ helloworld/formatting/formatting_test.go
@@ -3,6 +3,8 @@ package formatting_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"golang.kevinchen-verkada/helloworld/formatting"
 )
 
@@ -12,4 +14,18 @@ func TestFormattingUppercase(t *testing.T) {
 	if actual != expected {
 		t.Errorf("Expected %s, got %s", expected, actual)
 	}
+}
+
+func TestFormattingUppercaseWithBox(t *testing.T) {
+	expected := `┌─────────────────┐
+│                 │
+│ Verkada Codelab │
+│                 │
+│ HELLO THERE     │
+│                 │
+└─────────────────┘
+`
+	actual := formatting.FormatOutput("Hello there", true /* useBox */)
+	assert.Equal(t, expected, actual, "strings should be equal")
+
 }
